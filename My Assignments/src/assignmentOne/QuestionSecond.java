43b28a71809567657f4aa23a5550210616d1ef5a
TRON <TRON@DESKTOP-16JA64U.Dlink>
2022-07-26 19:32:25 +0530

    first 2 assignments

---
+++ My Assignments/src/assignmentOne/QuestionSecond.java
@@ -0,0 +1,21 @@
+package assignmentOne;
+
+public class QuestionSecond {
+
+	public static void main(String[] args) {
+int n =4;
+		
+		for (int i = 1; i<=n;i++) {
+			for(int j=0;j<n;j++) {
+				
+				System.out.print(i);
+				
+				
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
